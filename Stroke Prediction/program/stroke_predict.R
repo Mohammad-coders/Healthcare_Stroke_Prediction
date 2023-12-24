40ebd4eb0c375328058689567dadefe9d8d7b227
Mohammad Bin Harun <79358840+Mohammad-coders@users.noreply.github.com>
2023-12-24 20:33:46 +0600

    Update stroke_predict.R

--- Stroke Prediction/program/stroke_predict.R
+++ Stroke Prediction/program/stroke_predict.R
@@ -1,4 +1,7 @@
 library(readxl)
+library(dplyr)
+library(e1071)
+
 stroke<- read_excel("E:/AIUB/9th semester/Data Science/Final/Dataset/healthcare-dataset-stroke-data.xlsx")
 print(stroke)
 
@@ -6,9 +9,10 @@ names(stroke)
 summary(stroke)
 
 str(stroke)
+
 stroke$bmi[stroke$bmi=="N/A"]<- NA
 colSums(is.na(stroke))
-stroke$bmi<- as.integer(stroke$bmi) # convert char to integer
+stroke$bmi<- as.integer(stroke$bmi)
 
 str(stroke)
 
@@ -58,17 +62,132 @@ stroke$hypertension<-as.factor(stroke$hypertension)
 stroke$heart_disease<-as.factor(stroke$heart_disease)
 str(stroke)
 
-
-#function of corelation
-corelation<- function(attribute){
-  rel<- cor(stroke$stroke,stroke[[attribute]])
-  return(rel)
+Pearsons_Chi_squared<- function(attribute){
+  count_instances<- table(stroke[[attribute]],stroke$stroke)
+  p_value<- chisq.test(count_instances)
+  return(p_value)
 }
-corelation ('bmi')
-corelation ('avg_glucose_level')
-corelation('stroke')
-corelation('id')
+pearson_corelation_id <- Pearsons_Chi_squared('id')
+pearson_corelation_gender <- Pearsons_Chi_squared('gender')
+pearson_corelation_age <- Pearsons_Chi_squared('age')
+pearson_corelation_hypertension <- Pearsons_Chi_squared('hypertension')
+pearson_corelation_heart_disease <- Pearsons_Chi_squared('heart_disease')
+pearson_corelation_ever_married <- Pearsons_Chi_squared('ever_married')
+pearson_corelation_work_type <- Pearsons_Chi_squared('work_type')
+pearson_corelation_Residence_type <- Pearsons_Chi_squared('Residence_type')
+pearson_corelation_smoking_status <- Pearsons_Chi_squared('smoking_status')
+pearson_corelation_bmi <- Pearsons_Chi_squared('bmi')
+pearson_corelation_avg_glucose_level <- Pearsons_Chi_squared('avg_glucose_level')
+pearson_corelation_stroke <- Pearsons_Chi_squared('stroke')
+
+if(pearson_corelation_id$p.value<0.05){
+  print("There is a significant relationship between id and stroke.")
+}else{
+  stroke<- subset(stroke,select = -id)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_gender$p.value<0.05){
+  print("There is a significant relationship between gender and stroke.")
+}else{
+  stroke<- subset(stroke,select = -gender)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_age$p.value<0.05){
+  print("There is a significant relationship between age and stroke.")
+}else{
+  stroke<- subset(stroke,select = -age)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_hypertension$p.value<0.05){
+  print("There is a significant relationship between hypertension and stroke.")
+}else{
+  stroke<- subset(stroke,select = -hypertension)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_heart_disease$p.value<0.05){
+  print("There is a significant relationship between heart_disease and stroke.")
+}else{
+  stroke<- subset(stroke,select = -heart_disease)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_ever_married$p.value<0.05){
+  print("There is a significant relationship between ever married and stroke.")
+}else{
+  stroke<- subset(stroke,select = -ever_married)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_work_type$p.value<0.05){
+  print("There is a significant relationship between work type and stroke.")
+}else{
+  stroke<- subset(stroke,select = -work_type)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_Residence_type$p.value<0.05){
+  print("There is a significant relationship between residence type and stroke.")
+}else{
+  stroke<- subset(stroke,select = -Residence_type)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_smoking_status$p.value<0.05){
+  print("There is a significant relationship between smoking status and stroke.")
+}else{
+  stroke<- subset(stroke,select = -smoking_status)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_bmi$p.value<0.05){
+  print("There is a significant relationship between bmi and stroke.")
+}else{
+  stroke<- subset(stroke,select = -bmi)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_avg_glucose_level$p.value<0.05){
+  print("There is a significant relationship between avg glucose level and stroke.")
+}else{
+  stroke<- subset(stroke,select = -avg_glucose_level)
+  print("No significant relationship found.")
+}
+
+if(pearson_corelation_stroke$p.value<0.05){
+  print("There is a significant relationship between stroke  and stroke.")
+}else{
+  stroke<- subset(stroke,select = -stroke)
+  print("No significant relationship found.")
+}
+
+library(caret)
+
+set.seed(123)
+split_Index<- createDataPartition(stroke$stroke, p = 0.8, list = FALSE)
+train_data<- stroke[split_Index, ]
+test_data <- stroke[-split_Index, ]
+
+nb_model <- naiveBayes(stroke ~ ., data = train_data)
+predictions <- predict(nb_model, test_data)
+table(predictions)
+
+accuracy <- sum(predictions == test_data$stroke) / nrow(test_data)
+cat("Naive Bayes Accuracy:", round(accuracy,2), "\n")
 
 
-table(stroke$stroke)
-str(stroke)
+confusion_matrix<- table(predictions,test_data$stroke)
+
+recall_matrix<- confusion_matrix[2,2]/sum(confusion_matrix[2,])
+
+precision_matrix<- confusion_matrix[2,2]/sum(confusion_matrix[,2])
+
+f_measure <- 2 * (precision_matrix_second_class * recall_matrix_second_class) / (precision_matrix_second_class + recall_matrix_second_class)
+
+cat("Confusion Matrix: ",confusion_matrix,"\n")
+cat("Recall: ",recall_matrix_second_class,"\n")
+cat("Precision: ",precision_matrix_second_class,"\n")
+cat("f-measure: ",f_measure,"\n")
